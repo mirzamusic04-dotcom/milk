746e651f3854ce5fbf76b1ce884f60163d29660c
mirzamusic04-dotcom <mirza.music04@gmail.com>
2026-09-03 10:00:04 +0000

    vaja 2

--- vaje/uvod.py
+++ vaje/uvod.py
@@ -1,1 +1,17 @@
-print("Hello 1.Ri")
+
+def hello():
+    oddelek = input("Kateri oddelek si? ")
+    if oddelek.lower() == "1.ri":
+          print(f"Hello {oddelek} ")
+  
+def poštevanka():
+     x = int(input("izberi si število: "))
+     št = 1
+     while št <= 10:
+          print(f"{št} * {x} = {št * x}")
+          št += 1
+ 
+if __name__ == "__main__":
+    # To je moj prvi program
+    # hello()
+    poštevanka()
