93d570ba1d98a650a8fc49aa01a1c9e4d588d6c9
mirzamusic04-dotcom <mirza.music04@gmail.com>
2026-09-10 09:22:58 +0000

    nadeljeval sem vajo

--- vaje/uvod.py
+++ vaje/uvod.py
@@ -1,5 +1,5 @@
 
-def hello():
+"""def hello():
     oddelek = input("Kateri oddelek si? ")
     if oddelek.lower() == "1.ri":
           print(f"Hello {oddelek} ")
@@ -14,4 +14,49 @@ def poštevanka():
 if __name__ == "__main__":
     # To je moj prvi program
     # hello()
-    poštevanka()
+    poštevanka()"""
+
+"""x = 5
+y = 15
+z = -10
+
+# potenca
+print(10**3)
+
+# decimalna (float) števila
+
+x = 3.14
+Y = 10.012
+
+print(0.5 + 0.5 == 1)
+print((0.1 + 0.2) - 0.3)
+
+# string - niz znakov
+ime = "Luka"
+print(ime)
+print(len(ime))
+
+st = "22"
+print(st+st)
+print(st * 100)
+
+print(int(st) + 100)
+
+naslov = "kidričeva 55"
+
+print(naslov.upper())
+print(naslov.lower())
+naslov = naslov.strip()
+print(len(naslov))"""
+
+ime = "luka Colarič" #L.C
+ime = ime.upper() # LUKA COLARIČ
+spltIme = ime.split()
+print(type(spltIme))
+print(spltIme)
+
+ime = spltIme[0][0] #"LUKA"[0]
+pri = spltIme[1][0] #"COLARIČ"         altgr f+g
+
+print(ime, pri)
+print(f"{ime}.{pri}")
